a2fae1e2c1a68c5684cc40b4e45f36cb6fd0ab23
tmo-taka <45462276+tmo-taka@users.noreply.github.com>
2026-02-08 22:37:27 +0900

    feat: add type of User

--- src/domain/entities/Mention.ts
+++ src/domain/entities/Mention.ts
@@ -1,10 +1,11 @@
 import type { MentionStatus } from '../value-objects/MentionStatus';
+import type { User } from './User';
 
 export type Mention = {
   id: string;
   status: MentionStatus;
   message: string;
-  assignee?: string;
+  assignee?: User;
   createdBy: string;
   createdAt: string;
   updatedAt: string;
